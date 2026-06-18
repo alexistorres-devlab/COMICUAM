b9841382bcdf64e6564874f31d6e880819358323
IsaacClark24 <isaacmucino08@gmail.com>
2026-06-18 11:27:52 -0600

    Se agregaron atributos y metodos a la clase Persona

--- src/main/java/com/DSGE/ComicUAM/servicios/Persona.java
+++ src/main/java/com/DSGE/ComicUAM/servicios/Persona.java
@@ -1,5 +1,35 @@
 package com.dsge.comicuam.servicios;
 
 public class Persona {
-    
+    private int isUsuario;
+    private String nombre;
+    private String correo;
+    private String contraseña;
+    private Boolean Suscrito;
+    private Boolean status;
+    private String tarjeta;
+
+    public Persona(int isUsuario, String nombre, String correo, String contraseña, Boolean suscrito, Boolean status,
+            String tarjeta) {
+        this.isUsuario = isUsuario;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.contraseña = contraseña;
+        Suscrito = suscrito;
+        this.status = status;
+        this.tarjeta = tarjeta;
+    }
+
+    public boolean IniciarSesion(String correo, String contraseña) {
+        if (this.correo.equals(correo) && this.contraseña.equals(contraseña)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean VerificarSuscripcion() {
+        return this.Suscrito;
+    }
+
 }
