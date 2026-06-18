b980042442da69877f9684b30e7ea6a9aba00f1f
PMF99 <paulina.mendez@cua.uam.mx>
2026-06-18 11:29:03 -0600

    Incorporación de la clase Suscripcion

---
+++ src/main/java/com/DSGE/ComicUAM/servicios/Suscripcion.java
@@ -0,0 +1,17 @@
+package com.DSGE.ComicUAM.servicios;
+
+import java.time.LocalDate;
+
+import org.springframework.cglib.core.Local;
+
+public class Suscripcion {
+    private int idSuscripcion;
+    private String tipoPlan;
+    private LocalDate fechaInicio;
+    private LocalDate fechaVencimiento;
+    private String estado;
+
+    public void validacionAcceso(){}
+
+    public void cancelarPlan(){}
+}
